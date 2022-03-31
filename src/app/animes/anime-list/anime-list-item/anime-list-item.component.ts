bb6b90097dc73045e2283edc259e2bf5a26e3490
rtrydev <r.kulka44@gmail.com>
2022-03-31 17:36:37 +0200

    better mobile handle for cards

--- src/app/animes/anime-list/anime-list-item/anime-list-item.component.ts
+++ src/app/animes/anime-list/anime-list-item/anime-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { AnimeService } from 'src/app/anime.service';
 import { Anime } from '../../anime-model';
 
@@ -12,9 +12,17 @@ export class AnimeListItemComponent implements OnInit {
   @Input()
   anime: Anime = new Anime(0, '', '');
 
+  public innerWidth: any;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.innerWidth = window.innerWidth;
+  }
+
   constructor(private animeService: AnimeService) { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
   }
 
   onAnimeSelected(anime: Anime) {
