7d998d46e93dbed8eb0fde658e2821922a3cd4e9
rtrydev <r.kulka44@gmail.com>
2022-04-03 18:03:17 +0200

    add filter card

--- src/app/anime-search/anime-search.component.ts
+++ src/app/anime-search/anime-search.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Anime } from '../models/anime-model';
+import { Tag } from '../models/tag-model';
 import { AnimeService } from '../services/anime.service';
+import { TagService } from '../services/tag.service';
 
 @Component({
   selector: 'app-anime-search',
@@ -10,17 +13,30 @@ import { AnimeService } from '../services/anime.service';
 })
 export class AnimeSearchComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private animeService: AnimeService) { }
+  constructor(private route: ActivatedRoute, private animeService: AnimeService, private formBuilder: FormBuilder, private tagService: TagService) { }
+  animeFiltersForm = this.formBuilder.group({
+    'orderBy': 'None',
+    'demographics' : [],
+    'status': '',
+    'tagIds': []
+  });
 
-  anime: Anime[] = [];
+  onSubmit() {
+
+  }
+
+  tags: Tag[] = [];
+  animes: Anime[] = [];
 
   ngOnInit(): void {
+    this.tags = this.tagService.getTags();
+
     let params = this.route.snapshot.queryParams;
-    this.anime = this.animeService.getAnimes(params);
+    this.animes = this.animeService.getAnimes(params);
 
     this.route.queryParams.subscribe(
       queryParams => {
-        this.anime = this.animeService.getAnimes(queryParams);
+        this.animes = this.animeService.getAnimes(queryParams);
       }
     );
   }
