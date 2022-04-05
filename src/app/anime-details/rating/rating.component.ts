0163aebaa09de5cc445316cc20e4ef5af36898af
rtrydev <r.kulka44@gmail.com>
2022-04-05 14:58:26 +0200

    toggle rating enable when user is logged in

--- src/app/anime-details/rating/rating.component.ts
+++ src/app/anime-details/rating/rating.component.ts
@@ -1,27 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AnimeDetails } from 'src/app/models/anime-details-model';
 import { RatingService } from 'src/app/services/rating.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.sass']
 })
-export class RatingComponent implements OnInit {
+export class RatingComponent implements OnInit, OnDestroy {
 
   @Input()
   anime: AnimeDetails | null = null;
 
+  ratingEnabled: boolean = false;
+
+  private userSubscription: Subscription | null = null;
+
   ratingAvg: number = 0.0;
-  constructor(private ratingService: RatingService) { }
+  constructor(private ratingService: RatingService, private userService: UserService) { }
 
   ngOnInit(): void {
+
+    this.userSubscription = this.userService.currentUser.subscribe(
+      user => {
+        this.ratingEnabled = !!user;
+      }
+    )
+
     if(this.anime !== null){
       this.ratingService.getAvgRatingForAnime(this.anime?.id)
         .subscribe(result => {
           this.ratingAvg = result;
         })
     }
+  }
+  ngOnDestroy(): void {
+    this.userSubscription?.unsubscribe();
   }
 
 }
